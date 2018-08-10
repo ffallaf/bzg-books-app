74397417fb35e6d5c0930d3d9a47aa1a67dae6b8
Faiber Falla Mosquera <faiber.falla@gmail.com>
2018-08-10 09:51:27 -0500

    Se corrige problema generado con cambio de nombre de componente

--- src/app/collections/components/create-collection/create-collection.component.spec.ts
+++ src/app/collections/components/create-collection/create-collection.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AddCollectionComponent } from './create-collection.component';
+import { CreateCollectionComponent } from './create-collection.component';
 
 describe('AddCollectionComponent', () => {
-  let component: AddCollectionComponent;
-  let fixture: ComponentFixture<AddCollectionComponent>;
+  let component: CreateCollectionComponent;
+  let fixture: ComponentFixture<CreateCollectionComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AddCollectionComponent ]
+      declarations: [ CreateCollectionComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AddCollectionComponent);
+    fixture = TestBed.createComponent(CreateCollectionComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
