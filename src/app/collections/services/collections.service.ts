cf21c77983ceb7dd76829b781455b2d8d747e412
Faiber Falla Mosquera <faiber.falla@gmail.com>
2018-08-13 12:29:54 -0500

    Ajuste Coleciones

--- src/app/collections/services/collections.service.ts
+++ src/app/collections/services/collections.service.ts
@@ -4,6 +4,7 @@ import { AngularFireAuth } from "angularfire2/auth";
 import { AngularFireDatabase, AngularFireList } from "angularfire2/database";
 import * as firebase from "firebase";
 import { MessagesService } from '../../alerts/services/messages.service';
+import { Collection } from '../models/collection';
 
 @Injectable({
   providedIn: 'root'
@@ -16,14 +17,14 @@ export class CollectionsService {
   constructor(private authFire: AngularFireAuth, private rdb: AngularFireDatabase, private messageService: MessagesService) {
     this.authFire.authState.subscribe(
       user => {
-        this.user = user,
-        this.collectionsRef = this.rdb.list('collections/' + this.user.uid);
+        this.user = user
       }
     );
    }
 
    createCollection(collectionName: string): void {
-    let newCollection = { name: collectionName, books: new Array<any>()};
+    let newCollection = { name: collectionName, books: []};
+    this.collectionsRef = this.rdb.list('collections/' + this.user.uid + '/' + collectionName);
     const createPromise = this.collectionsRef.push(newCollection);
     createPromise.then(_ => this.messageService.message("Colección creada", "success"));
 
